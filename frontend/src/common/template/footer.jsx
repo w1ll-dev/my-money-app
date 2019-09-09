87b2a297f0927ccf316dc81e4e77464f0cce1156
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-09-09 10:28:14 -0300

    Footer component added.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; Will Márcio - 2019
+            <a href='https://www.linkedin.com/in/will-m%C3%A1rcio-84a082192/'> - LinkedIn</a>
+            <a href='https://github.com/w1ll-dev'> - GitHub</a>
+        </strong>
+    </footer>
+) 
+
+export default Footer
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className="wrapper">
         <Header/>
         <SideBar/>
+        <div className='content-wrapper'>
+            <h1>content</h1>
+        </div>
+        <Footer/>
     </div>
 )
