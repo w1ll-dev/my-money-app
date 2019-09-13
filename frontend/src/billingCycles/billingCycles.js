7b7588c1b4f4fc4540a6a78368ec15bf32dafb4c
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-09-13 10:50:35 -0300

    Tab header component created and added.

--- frontend/src/billingCycles/billingCycles.js
+++ frontend/src/billingCycles/billingCycles.js
@@ -5,6 +5,7 @@ import TabsHeader from '../common/tabs/tabsHeader';
 import TabsContent from '../common/tabs/tabsContent';
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import TabHeader from '../common/tabs/tabHeader';
 
 class BillingCycles extends Component { 
     render(){
@@ -14,7 +15,26 @@ class BillingCycles extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader
+                                label='  to list'
+                                icon = 'bars'
+                                target='tabList'
+                            />
+                            <TabHeader
+                                label='  include'
+                                icon = 'plus'
+                                target='tabCreate'
+                            />
+                            <TabHeader
+                                label='  update'
+                                icon = 'pencil'
+                                target='tabUpdate'
+                            />
+                            <TabHeader
+                                label='  exclude'
+                                icon = 'trash-o'
+                                target='tabRemove'
+                            />
                         </TabsHeader>
                         <TabsContent>
 
