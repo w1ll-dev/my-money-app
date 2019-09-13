6282795b0c33c9c38675c4b8fa7f6ab678364372
w1ll-dev <w1llmarcio.dev@hotmail.com>
2019-09-13 15:52:50 -0300

    Setup to view redux process added.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,8 @@ import promise from 'redux-promise'
 import App from '../src/main/app'
 import reducers from './main/reducers'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devtools)
 
 ReactDOM.render(
     <Provider store={store}>
